ee09ef6862bbef795bab26efca1e734c2d53c7d3
shju0317 <shju0317@naver.com>
2020-08-20 16:01:57 +0900

    SpMVC

---
+++ SpMVC_ShopV1/src/main/java/com/biz/shop/persistence/ProductDao.java
@@ -0,0 +1,9 @@
+package com.biz.shop.persistence;
+
+import java.util.List;
+
+import com.biz.shop.model.ProductVO;
+
+public interface ProductDao extends GenericDao<ProductVO, String>{
+	public List<ProductVO> findByTitle(String title);
+}
